9a856e8cb48f5db87e9b655f56b165355ff8e738
Nuñez Manrique Cuauhtémoc Emmanuel <76032890+NunezManriqueCuauhtemocEmmanuel@users.noreply.github.com>
2022-04-08 11:44:14 -0500

    beta 2

---
+++ js/sweetAlert.js
@@ -0,0 +1,9 @@
+Swal.fire({
+    title: "Bienvenido",
+    text: "Tus datos se han creado correctamente",
+    icon: 'success',
+    confirmButtonText: 'Confirmar',
+    backdrop: false,
+    background: '#031853',
+    color:  "#fff"
+});
